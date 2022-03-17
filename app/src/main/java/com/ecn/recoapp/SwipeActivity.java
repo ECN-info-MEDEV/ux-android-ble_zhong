31b851f359238ec8969b38fa2fb7331b72b83ac0
ZGChung <69182806+ZGChung@users.noreply.github.com>
2022-03-17 09:23:55 +0100

    update the imports for SwipeActivity

--- app/src/main/java/com/ecn/recoapp/SwipeActivity.java
+++ app/src/main/java/com/ecn/recoapp/SwipeActivity.java
@@ -2,7 +2,11 @@ package com.ecn.recoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.util.Log;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SwipeActivity extends AppCompatActivity {
 
